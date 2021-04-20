cd5849080e4d10926d8b2d8fd84c048ca01028ed
Kacper Bukowski <kacper.bukowski@student.wat.edu.pl>
2021-04-20 20:17:12 +0200

    Próba podłączenia bazy danych

--- raport.php
+++ raport.php
@@ -5,6 +5,8 @@
         <h2>Kosmici mnie uprowadzili - zgłoszenie porwania</h2>
         <link rel="stylesheet" href="style.css" type="text/css">
         <?php
+            $first_name = $_POST['firstname'];
+            $last_name = $_POST['lastname'];
             $name = $_POST['firstname'].' '.$_POST['lastname'];
             $when_it_happened = $_POST['whenithappened'] ;
             $email = $_POST['email'];
@@ -14,6 +16,15 @@
             $what_they_did = $_POST['whattheydid'] ;
             $fang_spotted = $_POST['fangspotted'] ;
             $other = $_POST['other'] ;
+
+            $dbc = mysqli_connect('localhost', 'root','','aliendb_wel18ec1s1') or die('Błąd podczas łączenia z bazą danych SQL.');
+
+            $query = "INSERT INTO aliens_abduction(first_name, last_name, when_it_happened, how_long, how_many, alien_description, what_they_did, fang_spotted, other, email) VALUES ('$first_name','$first_name','$last_name','$when_it_happened','$how_long', '$how_many', '$alien_description', '$what_they_did', '$fang_spotted', '$other', '$email')";
+
+            $result = mysqli_query($dbc, $query) or die ('Wystąpił problem podczas przetwarzania zapytań do bazy SQL');
+
+            mysqli_close($dbc);
+
             echo 'Zgłoszenie wygenerowane na prośbę użytkownika: <br/>';
             print "<h3>".$name."</h3>";
             echo '<br/> <b>Dziękujemy za przesłanie formularza.</b><br/>';
