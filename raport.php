d9a767e9679ade48f4302f6d1896de27dafc3127
Kacper Bukowski <kacper.bukowski@student.wat.edu.pl>
2021-04-20 19:36:33 +0200

    Poprawienie kodu php, teraz działa

--- raport.php
+++ raport.php
@@ -3,27 +3,28 @@
     </head>
     <body>
         <h2>Kosmici mnie uprowadzili - zgłoszenie porwania</h2>
+        <link rel="stylesheet" href="style.css" type="text/css">
         <?php
             $name = $_POST['firstname'].' '.$_POST['lastname'];
-            $how_many = $_POST['howmany'];
-            $what_they_did = $_POST['whattheydid'] ;
-            $other = $_POST=['other'] ;
             $when_it_happened = $_POST['whenithappened'] ;
+            $email = $_POST['email'];
             $how_long = $_POST['howlong'] ;
+            $how_many = $_POST['howmany'];
             $alien_description = $_POST['aliendescription'] ;
+            $what_they_did = $_POST['whattheydid'] ;
             $fang_spotted = $_POST['fangspotted'] ;
-            $email = $_POST['email'] ;
-            // implode(" ",$other);
-            echo $name;
-            echo '<br/> Dziękujemy za przesłanie formularza.<br/>';
-            echo 'Porwano cię:'.$when_it_happened;
-            echo ' i nie było Cię przez'.$how_long.'<br/>';
+            $other = $_POST['other'] ;
+            echo 'Zgłoszenie wygenerowane na prośbę użytkownika: <br/>';
+            print "<h3>".$name."</h3>";
+            echo '<br/> <b>Dziękujemy za przesłanie formularza.</b><br/>';
+            echo 'Porwano cię: '.$when_it_happened;
+            echo ' i nie było Cię przez '.$how_long.'<br/>';
             echo 'Liczba kosmitów: '.$how_many.'<br/>';
             echo 'Opis kosmitów:'.$alien_description.'<br/>';
             echo 'Działania kosmitów: '.$what_they_did.'<br/>';
-            echo 'Czy widziano Kła?'.$fang_spotted.'<br/>';
+            echo 'Czy widziano Kła: '.$fang_spotted.'<br/>';
             echo 'Inne komentarze: '.$other.'<br/>';
-            echo 'Adres e-mail:'.$email;
+            echo 'Adres e-mail: '.$email;
         ?>
     </body>
 </html>
